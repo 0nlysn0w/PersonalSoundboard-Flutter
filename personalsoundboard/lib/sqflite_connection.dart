0a7d0c77fc29aeaa3d1201ca4a4efc0ac41ead47
Ramon/Pc-home <ramon.meijers@hotmail.com>
2018-05-28 21:08:50 +0200

    SQLite startup, not tested yet

---
+++ personalsoundboard/lib/sqflite_connection.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class SQFLiteConnect{
+
+  SQFLiteConnect();
+  Future<String> connectionstring() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, "sounds.db");
+    return path;
+  }
+
+  Future<int> insertIntoTable(String title) async {
+    Database _db = await openDatabase(await connectionstring(), version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute(
+          "CREATE TABLE DBLSounds (id INTEGER PRIMARY KEY, title TEXT, image BLOB, sound BLOB);"
+        );
+      }
+    );
+    Map<String, dynamic> maps = new Map<String, dynamic>();
+
+    maps["title"] = title;
+    final m = _db.insert("DBLSounds", maps);
+    return m;
+  }
+
+}
